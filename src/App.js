89a944af6360647d8daebf5d3c9c6b45c64cbd2d
Abhishek kumar <aasalex21c@gmail.com>
2024-04-01 13:01:57 +0530

    working on it

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import LanguageSelect from './Components/LanguageSelect';
 import SuccessAndErrorMsg from './Components/SuccessAndErrorMsg';
 import { useState, useRef } from 'react';
 
-const Token = process.env.REACT_APP_X_RAPIDAPI_KEY_TEXT_TRANSLATOR2_BY_DICKYAGUSTIN;
+const API_KEY = process.env.REACT_APP_X_RAPIDAPI_KEY_TEXT_TRANSLATOR2_BY_DICKYAGUSTIN;
 
 
 function App() {
+  
   // setting states
     let [stateTranslatedText, udpateStateTranslatedText] = useState(null);
     let [stateLoaderImageHidden, updateStateLoaderImageHidden] = useState(true);
@@ -59,7 +60,10 @@ function App() {
       });
     }
     function handleUserTranslateRequest(event){
-        // Safeguard
+      // hidding output text field         
+        udpateStateTranslatedText(null);
+
+      // Safeguard
         if(stateIsSubmitBtnDisabled){
           return;
         }
@@ -82,15 +86,15 @@ function App() {
       encodedParams.set('target_language', refDestinationLang.current.value);
       encodedParams.set('text', refTextareaSrcLang.current.value);
       // console.log('src and dstinationlang',  refSrcLang.current.value, refDestinationLang.current.value,  refTextareaSrcLang.current.value);
-      console.log(Token)
-      return ;
+      // console.log(API_KEY)
+      // return ;
       
       const options = {
         method: 'POST',
         url: 'https://text-translator2.p.rapidapi.com/translate',
         headers: {
           'content-type': 'application/x-www-form-urlencoded',
-          'X-RapidAPI-Key': '',
+          'X-RapidAPI-Key': API_KEY,
           'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
         },
         data: encodedParams,
@@ -98,9 +102,14 @@ function App() {
       
       try {
         const response = await axios.request(options);
-        console.log(response.data);
+        let translatedText = response.data.data.translatedText;
+        console.log(translatedText);
+        // setting it as the value of textarea
+        // which making it visible
+        udpateStateTranslatedText(translatedText);
+
       } catch (error) {
-        console.error(error);
+        throw new Error(`failed to translate yours text! ${error}` );
       }      
     }
     async function makeAPICall(){
@@ -112,7 +121,7 @@ function App() {
           showLoaderImage();
           upateStateIsSubmitBtnDisabled(true);
           updateStateLabelSubmitButton('Translating...');
-  
+  // return;
           await axiosAPICall();
   
         // enable button
@@ -131,25 +140,25 @@ function App() {
 // returning jsx
 
   return (
-    <div className='mt-[2rem] pt-[1rem] border-2 border-slate-200 p-[2rem] w-[50rem]  m-auto rounded-md flex flex-col gap-[1rem] text-[1.2rem] text-slate-200 '>
+    <div id='wrapperUniversaLanguageTranslatorWebApp' className='mt-[2rem] pt-[1rem] border-2 border-slate-200 p-[2rem] w-[50rem]  m-auto rounded-md flex flex-col gap-[1rem] text-[1.2rem] text-slate-200 '>
       <h1 className=' text-[3rem] text-slate-50 flex gap-[1rem] justify-center items-center'>
         <i className="fa-duotone fa-language"></i>
-        <span className='text-[2rem] font-semibold smallCaps'>Universal Language Translator WebApp</span>
+        <span className='text-[2rem] font-semibold smallCaps appName'>Universal Language Translator WebApp</span>
       </h1>
       <LanguageSelect refernceHook={refSrcLang} supportedLangs={supportedLangs} name={'srcLang'} label={'Source Language'} defaultValue="en"/>
       <textarea ref={refTextareaSrcLang} rows={3} placeholder='Enter text You want to Translate' className='text-slate-900 transition focus:outline focus:outline-2 focus:outline-yellow-500 p-[1rem] rounded-md bg-stone-200'/>
       <LanguageSelect refernceHook={refDestinationLang} supportedLangs={supportedLangs} name={'destLang'} label={'Destination Language'}  defaultValue="hi"/>
-      <div className={`${stateLoaderImageHidden ? "displayNone" : "" } w-[25rem] h-[20rem] self-center `}>
+      <div id='wrapperLoaderImage' className={`${stateLoaderImageHidden ? "displayNone" : "" } w-[25rem] h-[20rem] self-center `}>
         <img src={require('./Assests/Images/loader.png')} className="object-none  rounded-xl shadow-yellow-300 shadow-2xl  w-[100%] h-[100%]"/>
       </div>  
       <SuccessAndErrorMsg  stateSuccessAndErrorMsg={stateSuccessAndErrorMsg}/>   
-      <button disabled={stateIsSubmitBtnDisabled} onClick={handleUserTranslateRequest} className={`select-none wrapperGeneratePassword mt-[1rem] flex gap-[1rem] items-center justify-center outline outline-2 outline-amber-50  hover:bg-yellow-400 transition cursor-pointer p-[1rem] rounded-md hover:text-stone-700 text-slate-900  bg-yellow-300  text-[2rem]  ${stateIsSubmitBtnDisabled === true? "disabled"  : "bg-yellow-300" } `}>
+      <button id='submitBtn' disabled={stateIsSubmitBtnDisabled} onClick={handleUserTranslateRequest} className={`select-none wrapperGeneratePassword mt-[1rem] flex gap-[1rem] items-center justify-center outline outline-2 outline-amber-50  hover:bg-yellow-400 transition cursor-pointer p-[1rem] rounded-md hover:text-stone-700 text-slate-900  bg-yellow-300  text-[2rem]  ${stateIsSubmitBtnDisabled === true? "disabled"  : "bg-yellow-300" } `}>
         <i className="fa-duotone fa-language"></i>
         <span className='font-semibold'>{stateLabelSubmitButton}</span>
         </button>
         {
         stateTranslatedText &&        
-          <textarea ref={refTextareaDestinationLang} rows={3} className='text-slate-900 transition focus:outline focus:outline-2 focus:outline-yellow-500 p-[1rem] rounded-md bg-stone-200' readOnly placeholder='Yours Translated text will be shown here'/>
+          <textarea  ref={refTextareaDestinationLang} rows={3} className='text-slate-900 transition focus:outline focus:outline-2 focus:outline-yellow-500 p-[1rem] rounded-md bg-stone-300' readOnly placeholder='Yours Translated text will be shown here' value={stateTranslatedText}/>
         }
     </div>
 
--- src/Components/LanguageSelect.jsx
+++ src/Components/LanguageSelect.jsx
@@ -1,6 +1,6 @@
 export default function LanguageSelect({supportedLangs, name, defaultValue, label, refernceHook}){
   return (
-    <div className="flex gap-[1rem] items-center">
+    <div className="wrapperLangSelect flex gap-[1rem] items-center">
       <label htmlFor={name} className="min-w-[12rem] text-right">{label}</label>
       <select ref={refernceHook} defaultValue={defaultValue} id={name} className="bg-slate-700 focus:outline-none focus:ring focus:ring-emerald-700 p-[1rem] pb-[.5rem] placeholder:text-slate-200 pt-[.5rem] rounded-md text-xl text-zinc-50">
       {
